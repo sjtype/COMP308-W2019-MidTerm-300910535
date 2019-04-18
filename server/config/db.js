2222dabb51419acf4fe57918dba03d76d0618a8e
sjtype <31806705+sjtype@users.noreply.github.com>
2019-04-18 14:54:50 -0400

    added github repository link

--- server/config/db.js
+++ server/config/db.js
@@ -1,3 +1,12 @@
+/*
+  File name: COMP308-W2019-Midterm-300910535
+  Author's name: Semy Jin
+  Student ID: 300910535
+  Web app name: Favourite Book List
+  Github repository link: https://github.com/sjtype/COMP308-W2019-MidTerm-300910535
+  Heroku link: https://comp308w2019-midterm-300910535.herokuapp.com/
+*/
+
 module.exports = {
   //local MongoDB deployment ->
   //"URI": "mongodb://localhost/books308"
--- server/routes/books.js
+++ server/routes/books.js
@@ -3,6 +3,7 @@
   Author's name: Semy Jin
   Student ID: 300910535
   Web app name: Favourite Book List
+  Github repository link: https://github.com/sjtype/COMP308-W2019-MidTerm-300910535
   Heroku link: https://comp308w2019-midterm-300910535.herokuapp.com/
 */
 
